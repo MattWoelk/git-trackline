a12f1cefe53a3076f49b097c7b0757b517e419d4
Matt Woelk <matt.woelk@gmail.com>
2013-01-11 13:54:12 -0600

    meow

--- track_line_over_commits.sh
+++ track_line_over_commits.sh
@@ -2,11 +2,14 @@
 
 ####EXAMPLE: git blame -n -s -C -L 439,+1 bridgeChart.js
 ####usage: ./track_line_over_commits.sh readme.txt 2
+NOW=`awk 'NR==3' $1`
+echo -------- $1 $2\) $NOW
 FIRS=`git blame -n -f -s -C -L $2,+1 $1`
 HASH=`echo $FIRS | awk '{print $1}'`
 NEWLINE=`echo $FIRS | awk '{print $3}'`
 echo $FIRS
-echo $HASH
-echo $NEWLINE
-SECO=`git blame -n -f -s -C -L $NEWLINE,+1 $HASH -- $1`
-echo $SECO
+FIRS=`git blame -n -f -s -C -L $NEWLINE,+1 $HASH^ -- $1`
+HASH=`echo $FIRS | awk '{print $1}'`
+NEWLINE=`echo $FIRS | awk '{print $3}'`
+echo $FIRS
+#FIRS=`git blame -n -f -s -C -L $NEWLINE,+1 $HASH^ -- $1`
